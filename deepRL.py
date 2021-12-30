f42e10dae00870c5653819254027a1f4cad53848
porat-ah <porat2001@gmail.com>
2021-12-30 17:33:05 +0200

    change the network size and checkpoint system and epsilon

--- deepRL.py
+++ deepRL.py
@@ -40,45 +40,35 @@ gamma = 0.999
 # Network
 state = tf.placeholder(tf.float32, shape= [1, 4, 4, 1])
 W_conv = tf.Variable(tf.truncated_normal(
-    shape= [3, 3, 1, 10], stddev= 1e-5))
-B_conv = tf.Variable(tf.constant(value= 0.0, shape= [10]))
+    shape= [3, 3, 1, 40], stddev= 1e-5))
+B_conv = tf.Variable(tf.constant(value= 0.0, shape= [40]))
 Z_conv = tf.nn.conv2d(state, W_conv, strides= [1, 1, 1, 1], padding= "SAME") + B_conv
 A_conv = tf.nn.silu(Z_conv)
-# W1_conv = tf.Variable(tf.truncated_normal(
-#     shape= [2, 2, 10, 5], stddev= 1e-5))
-# B1_conv = tf.Variable(tf.constant(value= 0.0 , shape= [5] ))
-# Z1_conv = Z_conv = tf.nn.conv2d(A_conv, W1_conv, strides= [1, 1, 1, 1], padding= "SAME") + B1_conv
-# A1_conv = tf.nn.silu(Z1_conv)
 A_conv_flat = tf.reshape(A_conv, [1, -1])
 W1 = tf.Variable(tf.truncated_normal(
-    shape = [A_conv_flat.shape[1], 4], stddev=1e-5))
-b1 = tf.Variable(tf.constant(0.0, shape=[4]))
+    shape = [A_conv_flat.shape[1], 4_00], stddev=1e-5))
+b1 = tf.Variable(tf.constant(0.0, shape=[4_00]))
 z1 = tf.matmul(A_conv_flat, W1) + b1
 a1 = tf.nn.silu(z1)
 W2 = tf.Variable(tf.truncated_normal(
-    shape = [4, 100], stddev=1e-5))
-b2 = tf.Variable(tf.constant(0.0, shape=[100]))
+    shape = [4_00, 1_00], stddev=1e-5))
+b2 = tf.Variable(tf.constant(0.0, shape=[1_00]))
 z2 = tf.matmul(a1, W2) + b2
 a2 = tf.nn.silu(z2)
 W3 = tf.Variable(tf.truncated_normal(
-    shape = [100, 1000], stddev=1e-5))
-b3 = tf.Variable(tf.constant(0.0, shape=[1000]))
+    shape = [1_00, 50], stddev=1e-5))
+b3 = tf.Variable(tf.constant(0.0, shape=[50]))
 z3 = tf.matmul(a2, W3) + b3
 a3 = tf.nn.silu(z3)
 W4 = tf.Variable(tf.truncated_normal(
-    shape = [1000, 5000], stddev=1e-5))
-b4 = tf.Variable(tf.constant(0.0, shape=[5000]))
+    shape = [50, 10], stddev=1e-5))
+b4 = tf.Variable(tf.constant(0.0, shape=[10]))
 z4 = tf.matmul(a3, W4) + b4
 a4 = tf.nn.silu(z4)
 W5 = tf.Variable(tf.truncated_normal(
-    shape = [5000, 400], stddev=1e-5))
-b5 = tf.Variable(tf.constant(0.0, shape=[400]))
-z5 = tf.matmul(a4, W5) + b5
-a5 = tf.nn.silu(z5)
-W6 = tf.Variable(tf.truncated_normal(
-    shape = [400, 4], stddev=1e-5))
-b6 = tf.Variable(tf.constant(0.0, shape=[4]))
-Q4actions = tf.matmul(a5, W6) + b6
+    shape = [10, 4], stddev=1e-5))
+b5 = tf.Variable(tf.constant(0.0, shape=[4]))
+Q4actions = tf.matmul(a4, W5) + b5
 Q_n = tf.placeholder(tf.float32, shape=[1, 4])
 loss = tf.pow(Q4actions - Q_n, 2)
 update = tf.train.AdamOptimizer(1e-5).minimize(loss)
@@ -89,9 +79,13 @@ sess.run(tf.global_variables_initializer())
 # new_saver.restore(sess, tf.train.latest_checkpoint('./RL_output/'))
 
 r = 0
-for i in range(1,10_001):
-    # epsilon = EPS/i
+for i in range(1, 10_001):
     epsilon = 0.1
+    if i % 100 >= 90:
+        epsilon = 0
+    if i % 100 == 0:
+        saver = tf.compat.v1.train.Saver()
+        save_path = saver.save(sess, f"./RL_output/model_{i}")
     print('new game , game number :', i)
     tiles_rect = Rect(*TILES_RECT_POS, TILES_RECT_SIZE, TILES_RECT_SIZE)
     tiles = Tiles(TILES_RECT_POS, TILES_RECT_SIZE)
@@ -138,10 +132,11 @@ for i in range(1,10_001):
             r += (points**2)
             print('last points:', points)
             mavg = d_factor*mavg + (1-d_factor)*points
+            print("mavg : " , mavg)
             plot.plot(mavg)
 
         elif not game.changed:
-            r -= 0.1
+            r -= 100
 
         Q_corrected = np.copy(all_Qs)
         next_Q = sess.run(Q4actions, feed_dict={
@@ -149,10 +144,9 @@ for i in range(1,10_001):
 
         Q_corrected[0][next_action] = r + gamma * np.max(next_Q)
         sess.run(update, feed_dict={state: reshape(s), Q_n: Q_corrected})
-        s = s_n # move to the next state
+        s = s_n #move to the next state
         if SHOW:
             pygame.display.update()
             clock.tick(60)
 
-saver = tf.compat.v1.train.Saver()
-save_path = saver.save(sess, "./RL_output/model")
+
